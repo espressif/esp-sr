c3d120ac9d94b16744e76aea95366a48b9a10be1
sxy <sunxiangyu@espressif.com>
2021-09-24 21:29:56 +0800

    feat(multinet): add API to modify speech commands online

--- speech_command_recognition/tool/multinet_g2p.py
+++ speech_command_recognition/tool/multinet_g2p.py
@@ -5,19 +5,28 @@ import pandas
 
 def english_g2p(text, alphabet=None):
     g2p = G2p()
-    labels = g2p(text)
-    out = []
+    out = ""
 
     if alphabet is None:
         alphabet={"AE1": "a", "N": "N", " ": " ", "OW1": "b", "V": "V", "AH0": "c", "L": "L", "F": "F", "EY1": "d", "S": "S", "B": "B", "R": "R", "AO1": "e", "D": "D", "AH1": "c", "EH1": "f", "OW0": "b", "IH0": "g", "G": "G", "HH": "h", "K": "K", "IH1": "g", "W": "W", "AY1": "i", "T": "T", "M": "M", "Z": "Z", "DH": "j", "ER0": "k", "P": "P", "NG": "l", "IY1": "m", "AA1": "n", "Y": "Y", "UW1": "o", "IY0": "m", "EH2": "f", "CH": "p", "AE0": "a", "JH": "q", "ZH": "r", "AA2": "n", "SH": "s", "AW1": "t", "OY1": "u", "AW2": "t", "IH2": "g", "AE2": "a", "EY2": "d", "ER1": "k", "TH": "v", "UH1": "w", "UW2": "o", "OW2": "b", "AY2": "i", "UW0": "o", "AH2": "c", "EH0": "f", "AW0": "t", "AO2": "e", "AO0": "e", "UH0": "w", "UH2": "w", "AA0": "n", "AY0": "i", "IY2": "m", "EY0": "d", "ER2": "k", "OY2": "u", "OY0": "u"}
 
-    for item in labels:
-        if item not in alphabet:
-            print("skip %s, not found in alphabet")
-            continue
-        else:
-            out.append(alphabet[item])
-    print(text, " -> ", out)
+    text_list = text.split(";")
+    for item in text_list:
+        item = item.split(",")
+        for phrase in item:
+            labels = g2p(phrase)
+            for char in labels:
+                if char not in alphabet:
+                    print("skip %s, not found in alphabet")
+                    continue
+                else:
+                    out += alphabet[char]
+            if phrase != item[-1]:
+                out += ','
+        out += ";"
+
+    print("in:", text)
+    print("out:", out)
     
     return out
 
