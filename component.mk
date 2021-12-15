9016ba6b6c58b7c791755dd77833d36e178aa4ba
Wang Wang Wang <wangwangwang@espressif.com>
2021-12-15 15:26:42 +0800

    system(esp32): Support make on esp32

--- component.mk
+++ component.mk
@@ -1,24 +1,14 @@
-COMPONENT_ADD_INCLUDEDIRS := lib/include \
-                             wake_word_engine/include \
-                             speech_command_recognition/include \
-                             acoustic_algorithm/include \
+COMPONENT_ADD_INCLUDEDIRS := include/$(IDF_TARGET) \
                              esp-tts/esp_tts_chinese/include \
 
-COMPONENT_SRCDIRS := speech_command_recognition
+COMPONENT_SRCDIRS := src
 
 
-LIB_FILES := $(shell ls $(COMPONENT_PATH)/wake_word_engine/lib*.a) \
-             $(shell ls $(COMPONENT_PATH)/lib/lib*.a) \
-             $(shell ls $(COMPONENT_PATH)/acoustic_algorithm/lib*.a) \
-             $(shell ls $(COMPONENT_PATH)/speech_command_recognition/lib*.a) \
-             $(shell ls $(COMPONENT_PATH)/esp-tts/esp_tts_chinese/lib*.a) \
+LIB_FILES := $(shell ls $(COMPONENT_PATH)/lib/$(IDF_TARGET)/lib*.a) \
+             $(shell ls $(COMPONENT_PATH)/esp-tts/esp_tts_chinese/lib*.a)
 
 LIBS := $(patsubst lib%.a,-l%,$(LIB_FILES))
 
-COMPONENT_ADD_LDFLAGS +=  -L$(COMPONENT_PATH)/lib \
-                          -L$(COMPONENT_PATH)/wake_word_engine \
-                          -L$(COMPONENT_PATH)/speech_command_recognition \
-                          -L$(COMPONENT_PATH)/acoustic_algorithm \
+COMPONENT_ADD_LDFLAGS +=  -L$(COMPONENT_PATH)/lib/$(IDF_TARGET) \
                           -L$(COMPONENT_PATH)/esp-tts \
                           $(LIBS)
-
