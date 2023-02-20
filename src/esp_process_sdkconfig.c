e9763db0787bffbbf01b85041f223e79ff4a4be5
sxy <sunxiangyu@espressif.com>
2023-02-20 11:16:17 +0800

    bugfix(mn): keep compatible with MultiNet5

--- src/esp_process_sdkconfig.c
+++ src/esp_process_sdkconfig.c
@@ -874,6 +874,10 @@ char *get_id_name_en(int i)
 
 esp_mn_error_t* esp_mn_commands_update_from_sdkconfig(const esp_mn_iface_t *multinet,  model_iface_data_t *model_data)
 {
+#ifdef CONFIG_SR_MN_EN_MULTINET6_CTC_QUANT || CONFIG_SR_MN_EN_MULTINET6_QUANT
+    return NULL;
+#endif
+
     esp_mn_commands_alloc();
     printf("esp_mn_commands_update_from_sdkconfig\n");
     int total_phrase_num = 0;
