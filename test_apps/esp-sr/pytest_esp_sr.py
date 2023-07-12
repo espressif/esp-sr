8bebc8d77808df30b8f739e1e08439b78d2dff48
xysun <sunxiangyu@espressif.com>
2023-07-12 14:57:00 +0800

    feat: Add ci for chinese on esp32s3

--- test_apps/esp-sr/pytest_esp_sr.py
+++ test_apps/esp-sr/pytest_esp_sr.py
@@ -1,19 +1,21 @@
 import pytest
 from pytest_embedded import Dut
 
-# @pytest.mark.target('esp32s3')
-# @pytest.mark.parametrize(
-#     'config',
-#     [
-#         'mn5q8_cn',
-#         'mn6_cn',
-#     ],
-# )
-# def test_multinet(dut: Dut)-> None:
-#     dut.run_all_single_board_cases(group="mn")
+@pytest.mark.target('esp32s3')
+@pytest.mark.env('esp32s3')
+@pytest.mark.parametrize(
+    'config',
+    [
+        'mn5q8_cn',
+        'mn6_cn',
+    ],
+)
+def test_multinet(dut: Dut)-> None:
+    dut.run_all_single_board_cases(group="mn")
 
 
 @pytest.mark.target('esp32s3')
+@pytest.mark.env('esp32s3')
 @pytest.mark.parametrize(
     'config',
     [
@@ -26,6 +28,7 @@ def test_wakenet(dut: Dut)-> None:
 
 
 @pytest.mark.target('esp32s3')
+@pytest.mark.env('esp32s3')
 @pytest.mark.parametrize(
     'config',
     [
