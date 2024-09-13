78ecd4110c6182509c32efa5b68cf328154066fd
xysun <sunxiangyu@espressif.com>
2024-09-13 20:06:32 +0800

    ci: support esp32p4

--- test_apps/esp-tts/pytest_esp_tts.py
+++ test_apps/esp-tts/pytest_esp_tts.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_embedded import Dut
 
+
 @pytest.mark.target('esp32s3')
 @pytest.mark.env('esp32s3')
 @pytest.mark.parametrize(
@@ -10,6 +11,20 @@ from pytest_embedded import Dut
     ],
 )
 def test_tts(dut: Dut)-> None:
+    # dut.run_all_single_board_cases(group="tts")
+    dut.expect_exact('Press ENTER to see the list of tests.')
+    dut.write('[tts]')
+    dut.expect_unity_test_output(timeout = 1000)
+
+@pytest.mark.target('esp32p4')
+@pytest.mark.env('esp32p4')
+@pytest.mark.parametrize(
+    'config',
+    [
+        'p4',
+    ],
+)
+def test_tts_p4(dut: Dut)-> None:
     # dut.run_all_single_board_cases(group="tts")
     dut.expect_exact('Press ENTER to see the list of tests.')
     dut.write('[tts]')
