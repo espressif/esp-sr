d72ed551cb1ebb628f2f41b00351d36bd6462d9a
xysun <sunxiangyu@espressif.com>
2024-12-31 20:26:04 +0800

    feat(esp32p4): add vadnet1

--- include/esp32p4/esp_afe_sr_models.h
+++ include/esp32p4/esp_afe_sr_models.h
@@ -4,7 +4,6 @@
 extern "C" {
 #endif
 
-#if defined CONFIG_USE_AFE
 #include "esp_afe_sr_iface.h"
 
 
@@ -18,17 +17,6 @@ extern const esp_afe_sr_iface_t esp_afe_vc_v1;
 #error No valid afe selected.
 #endif
 
-
-#else
-
-
-#include "esp_afe_sr_iface.h"
-extern const esp_afe_sr_iface_t esp_afe_sr_v1;
-extern const esp_afe_sr_iface_t esp_afe_vc_v1;
-#define ESP_AFE_SR_HANDLE esp_afe_sr_v1
-#define ESP_AFE_VC_HANDLE esp_afe_vc_v1
-
-#endif
 
 #ifdef __cplusplus
 }
