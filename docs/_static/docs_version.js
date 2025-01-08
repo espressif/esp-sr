8d6cb7c93c4de9f1e5c2df633a45c9e570ff738c
xysun <sunxiangyu@espressif.com>
2025-01-08 11:07:01 +0800

    docs: update esp32p4 benchmark

--- docs/_static/docs_version.js
+++ docs/_static/docs_version.js
@@ -1,12 +1,13 @@
 var DOCUMENTATION_VERSIONS = {
   DEFAULTS: { has_targets: false,
-              supported_targets: [ "esp32" ]
+              supported_targets: [ "esp32s3" ]
             },
   VERSIONS: [
-    { name: "latest", has_targets: true, supported_targets: [ "esp32", "esp32s3" ] },
+    { name: "latest", has_targets: true, supported_targets: [ "esp32", "esp32s3", "esp32p4" ] },
   ],
   IDF_TARGETS: [
      { text: "ESP32", value: "esp32"},
      { text: "ESP32-S3", value: "esp32s3"},
+     { text: "ESP32-P4", value: "esp32p4"},
   ]
 };
