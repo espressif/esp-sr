b4f12005532ef6deed00fc541901b72d5729d262
xysun <sunxiangyu@espressif.com>
2025-02-17 16:28:08 +0800

    dump esp-sr v2.0.1

--- include/esp32p4/esp_afe_aec.h
+++ include/esp32p4/esp_afe_aec.h
@@ -2,9 +2,8 @@
 #ifndef _ESP_AFE_AEC_H_
 #define _ESP_AFE_AEC_H_
 
-
-#include "esp_afe_config.h"
 #include "esp_aec.h"
+#include "esp_afe_config.h"
 
 #include <stdint.h>
 
@@ -13,19 +12,19 @@ extern "C" {
 #endif
 
 typedef struct {
-    aec_handle_t* handle;
+    aec_handle_t *handle;
     aec_mode_t mode;
     afe_pcm_config_t pcm_config;
     int frame_size;
-    int16_t  *data;
-}afe_aec_handle_t;
-
+    int16_t *data;
+} afe_aec_handle_t;
 
 /**
- * @brief Creates an instance to the AEC structure. 
- * 
- * @warning Currently only support 1 microphone channel and 1 playback channe. 
- * If input has multiple microphone channels and playback channels, just the first microphone channel and playback channel will be selected.
+ * @brief Creates an instance to the AEC structure.
+ *
+ * @warning Currently only support 1 microphone channel and 1 playback channe.
+ * If input has multiple microphone channels and playback channels, just the first microphone channel and playback
+ * channel will be selected.
  *
  * The input format, same as afe config:
  * M to represent the microphone channel
@@ -37,7 +36,8 @@ typedef struct {
  *
  * @param input_format     The input format
  * @param filter_length    The length of filter. The larger the filter, the higher the CPU loading.
- *                         Recommended filter_length = 4 for esp32s3 and esp32p4. Recommended filter_length = 2 for esp32c5.
+ *                         Recommended filter_length = 4 for esp32s3 and esp32p4. Recommended filter_length = 2 for
+ * esp32c5.
  * @param type             The type of afe, AFE_TYPE_SR or AFE_TYPE_VC
  * @param mode             The mode of afe, AFE_MODE_LOW_COST or AFE_MODE_HIGH_PERF
  *
@@ -45,17 +45,17 @@ typedef struct {
  */
 afe_aec_handle_t *afe_aec_create(const char *input_format, int filter_length, afe_type_t type, afe_mode_t mode);
 
-
 /**
  * @brief Performs echo cancellation a frame, based on the audio sent to the speaker and frame from mic.
- * 
+ *
  * @param inst        The instance of AEC.
- * @param indata      Input audio data, format is define by input_format. Note indata will be modified in function call.
- * @param outdata     Returns near-end signal with echo removed. 
+ * @param indata      Input audio data, format is define by input_format.
+ * @param outdata     Near-end signal with echo removed.  outdata must be 16-bit aligned.
+ *                    please use heap_caps_aligned_calloc(16, n, size, caps) to allocate an aligned chunk of memory
 
  * @return The bytes of outdata.
  */
-size_t afe_aec_process(afe_aec_handle_t *handel, int16_t *indata, int16_t *outdata);
+size_t afe_aec_process(afe_aec_handle_t *handel, const int16_t *indata, int16_t *outdata);
 
 /**
  * @brief Get frame size of AEC (the samples of one frame)
@@ -63,7 +63,6 @@ size_t afe_aec_process(afe_aec_handle_t *handel, int16_t *indata, int16_t *outda
  * @return Frame size
  */
 int afe_aec_get_chunksize(afe_aec_handle_t *handle);
-
 
 /**
  * @brief Free the AEC instance
